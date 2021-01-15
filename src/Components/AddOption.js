ef53929f3c785d6381847cb2b768b4cb54dbd494
Saim Ahmad <saimahmad1234@gmail.com>
2021-01-14 22:41:49 -0500

    added devtools and devServer in webpack. Also added "transform-class-properties" - babel plugin.

--- src/Components/AddOption.js
+++ src/Components/AddOption.js
@@ -2,40 +2,43 @@ import React from "react";
 
 export default class AddOption extends React.Component{
 
-    constructor(props){
-      super(props);
-      this.handleAddOption = this.handleAddOption.bind(this);
-      this.state = {
-        error: undefined
-      };
+  state = {
+    error: undefined
+  };
+  // constructor(props){
+  //   super(props);
+  //   this.handleAddOption = this.handleAddOption.bind(this);
+  //   this.state = {
+  //     error: undefined
+  //   };
+  // }
+
+  handleAddOption = (e) =>{
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    let errorMsg = this.props.addOption(option);
+    
+    if(!errorMsg){
+      e.target.elements.option.value = '';
     }
-  
-    handleAddOption(e){
-      e.preventDefault();
-      const option = e.target.elements.option.value.trim();
-      let errorMsg = this.props.addOption(option);
-      
-      if(!errorMsg){
-        e.target.elements.option.value = '';
-      }
-      // this.setState(() =>{
-      //   return{
-      //     error: errorMsg
-      //   };
-      // });
-      this.setState(()=>({error:errorMsg}));
-    }
-  
-    render(){
-      return(
-        <div>
-          {this.state.error && <p>{this.state.error}</p>}
-          <form onSubmit = {this.handleAddOption}>
-          <input type="text" name="option" />
-          <button >Add option</button>
-        </form>
-        </div>
-      );
-    }
+    // this.setState(() =>{
+    //   return{
+    //     error: errorMsg
+    //   };
+    // });
+    this.setState(()=>({error:errorMsg}));
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit = {this.handleAddOption}>
+        <input type="text" name="option" />
+        <button >Add option</button>
+      </form>
+      </div>
+    );
+  }
   }
 
